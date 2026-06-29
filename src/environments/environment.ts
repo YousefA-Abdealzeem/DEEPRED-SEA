456f4c25ec628033a7f7289d34f3e5d1cb4e6941
Yousef Alian <yousef227d@gmail.com>
2026-06-29 16:58:07 +0300

    fix: http

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://divigo.runasp.net/api/v1',
-  centerApiUrl: 'https://divigo.runasp.net/api/v1'
+  apiUrl: 'http://divigo.runasp.net/api/v1',
+  centerApiUrl: 'http://divigo.runasp.net/api/v1'
 };
